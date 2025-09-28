d743440370d72bc10b34eb88e52123f415a866ce
Josef Utbult <josef@utbult.design>
2025-09-28 19:49:49 +0200

    Fix example

--- examples/trace.rs
+++ examples/trace.rs
@@ -1,4 +1,8 @@
-use trace::{trace, trace_error, trace_error_once, trace_info, trace_info_once, trace_once, trace_panic, trace_setup, trace_warning, trace_warning_once, traceln, traceln_once, TraceHandler};
+use trace::{
+    TraceHandler, trace, trace_debug, trace_debug_once, trace_error, trace_error_once, trace_info,
+    trace_info_once, trace_once, trace_panic, trace_setup, trace_warning, trace_warning_once,
+    traceln, traceln_once,
+};
 
 struct MyTraceHandler {}
 
@@ -13,6 +17,7 @@ fn main() {
 
     trace!("I am a {} trace\n", "normal");
     traceln!("I am a {} trace", "normal newline");
+    trace_debug!("I am a {} trace", "debug");
     trace_info!("I am a {} trace", "info");
     trace_warning!("I am a {} trace", "warning");
     trace_error!("I am a {} trace", "error");
@@ -21,6 +26,7 @@ fn main() {
     for _ in 0..2 {
         trace_once!("I am a {} trace\n", "normal once");
         traceln_once!("I am a {} trace", "normal newline once");
+        trace_debug_once!("I am a {} trace", "debug once");
         trace_info_once!("I am a {} trace", "info once");
         trace_warning_once!("I am a {} trace", "warning once");
         trace_error_once!("I am a {} trace", "error once");
