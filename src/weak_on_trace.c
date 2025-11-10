649a5b27040e89df3a87d9fcbd7f7b654e189718
Josef Utbult <josef@utbult.design>
2025-11-10 22:23:30 +0100

    Try with adding a c file

--- src/weak_on_trace.c
+++ src/weak_on_trace.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
 
-// Use GCC/Clang weak attribute
+// Use GCC/Clang weak attribute to circumvent not having weak rust linking in
+// stable. This should be updated when that is a stable feature, as this is a
+// hack
 __attribute__((weak)) void _on_trace(unsigned int, const unsigned char*, size_t) {}
