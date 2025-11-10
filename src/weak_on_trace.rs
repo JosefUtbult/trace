bda3266498da4f2fbe0b01f3c2421b0ee92f12f0
Josef Utbult <josef@utbult.design>
2025-11-10 21:59:11 +0100

    Try to fix weak dummy stub

--- src/weak_on_trace.rs
+++ src/weak_on_trace.rs
@@ -1,4 +1,3 @@
-#[cfg(target_arch = "arm")]
 use core::arch::global_asm;
 
 // Create a weak symbol for _on_trace for ARM compilers. This should be updated to a #[weak]
@@ -14,7 +13,12 @@ _on_trace:
 "#
 );
 
-// Dummy stub for non-ARM targets (e.g., Linux)
-#[cfg(all(not(target_arch = "arm"), not(test)))]
-#[unsafe(no_mangle)]
-pub unsafe extern "Rust" fn _on_trace(_level: u8, _msg: &str) {}
+#[cfg(not(target_arch = "arm"))]
+global_asm!(
+    r#"
+    .weak _on_trace
+    .type _on_trace, @function
+_on_trace:
+    ret
+"#
+);
